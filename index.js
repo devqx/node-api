dcb9ac48e3b8f8d2381bfcef8255188253090684
Paul Oluwaseun <paul.softcelent@gmail.com>
2019-06-13 09:02:21 +0100

    added about route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app = express();
 app.get('/', function (req, res) {
 res.send('hello paul, it works!');
 });
+
+app.get('/about', (req,res)=>{
+    res.send({page:'about'});
+});
+
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 
